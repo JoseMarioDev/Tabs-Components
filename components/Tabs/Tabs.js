3abf8ab4cce70e03f41ad0e6035e7e0de4eef50e
Jose Montero Jr <josemariodev@gmail.com>
2019-05-16 02:59:16 -0500

    completed tabitem class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -48,10 +48,11 @@ class TabItem {
     const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    // Add a class named "tabs-item-selected" to this element
-    items.forEach(el => {
-      el.classList.remove('tabs-item-selected');
+    items.forEach(item => {
+      item.classList.remove('tabs-item-selected');
     });
+
+    // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
   }
 }
